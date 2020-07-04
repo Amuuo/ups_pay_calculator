cdff5a6321b66fa425bf22a4a4f3d69ae7a8e6a3
Adam Williams <adamthewilliams@gmail.com>
2020-07-04 06:10:35 -0500

    jquery

--- index.php
+++ index.php
@@ -2,13 +2,30 @@
 <html>
   <head>
    <link rel="stylesheet" href="css/index.css">
-   <script src="d3.js"></script>
+   <link href="jQueryAssets/jquery.ui.core.min.css" rel="stylesheet" type="text/css">
+   <link href="jQueryAssets/jquery.ui.theme.min.css" rel="stylesheet" type="text/css">
+   <link href="jQueryAssets/jquery.ui.button.min.css" rel="stylesheet" type="text/css">
+<script src="d3.js"></script>
+<script src="jQueryAssets/jquery-1.11.1.min.js"></script>
+<script src="jQueryAssets/jquery.ui-1.10.4.button.min.js"></script>
   </head>
-  <body>    
+  <body>
     <header>
       <img id="logo" src="img/ups_logo.png">
     </header>  
-    <main></main>
+    <main>
+  <div id="Checkboxes1">
+      <input type="checkbox" id="Checkbox1">
+      <label for="Checkbox1">Hourly</label>
+      <input type="checkbox" id="Checkbox2">
+      <label for="Checkbox2">Management</label>
+    </div>    
+    </main>
+  <script type="text/javascript">
+    $(document).ready(() => {
+	    $( "#Checkboxes1" ).buttonset(); 
+    });
+    </script>
   </body>  
 </html>
 <script src="js/index.js"></script>
