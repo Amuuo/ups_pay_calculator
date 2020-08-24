657705b546c95e661da0c2fe60ce23ed30940a80
Adam Williams <adamthewilliams@gmail.com>
2020-08-24 00:06:37 -0500

    moved global functions from index.js to index_functions.js

--- js/index.js
+++ js/index.js
@@ -7,28 +7,6 @@ let   MAIN_SHIFT       = new Shift(CURRENT_WORKDAY)
 
 
 
-const insert_register_link = () => {
-  
-  $register_link = $('<a>')
-                    .text('Register')
-                    .attr('href', './register.php')
-                    .attr('id', 'register_link');
-  
-  $('#footer').prepend($register_link);
-  //$register_link.insertBefore($('#footer'));
-}
-
-
-
-const setup_shift_submit_event_handler = () => {
-  
-  $('.shift_submit_button')
-    .click(() => {
-      CURRENT_WORKDAY.insertShiftAndCalculatePayBreakdown();                              
-    });
-}
-
-
 
 
 
