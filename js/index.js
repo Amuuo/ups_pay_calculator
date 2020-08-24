11d2864a39e088169cb35c300ca377a52cf32e07
Adam Williams <adamthewilliams@gmail.com>
2020-08-24 00:26:24 -0500

    removed submit button from everything

--- js/index.js
+++ js/index.js
@@ -12,18 +12,11 @@ let   MAIN_SHIFT       = new Shift(CURRENT_WORKDAY)
 
 $('document').ready(() => {
   
-  PAY_RATE = $('#hourly_rate_input').val();
-  insert_register_link();
-  setup_shift_submit_event_handler();
+  PAY_RATE = $('#hourly_rate_input').val()
+  insert_register_link()
+  setup_shift_submit_event_handler()
  
-  
-  $('#hide_employee_button')
-    .click(() => {
-        $('#employee_table2').remove();
-      }
-    )
-    .attr('id', 'show_employee_button');
-});
+})
   
 
 
--- js/index_functions.js
+++ js/index_functions.js
@@ -8,15 +8,6 @@ const insert_register_link = () => {
                     .attr('id', 'register_link');
   
   $('#footer').prepend($register_link);
-  //$register_link.insertBefore($('#footer'));
+
 }
 
-
-
-const setup_shift_submit_event_handler = () => {
-  
-  $('.shift_submit_button')
-    .click(() => {
-      CURRENT_WORKDAY.insertShiftAndCalculatePayBreakdown();                              
-    });
-}
