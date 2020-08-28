7512c500bc30b638d76b780719af62d28866d78a
Adam Williams <adamthewilliams@gmail.com>
2020-08-28 03:07:06 -0500

    all jquery removed from index_classes.js

--- js/index_classes.js
+++ js/index_classes.js
@@ -20,10 +20,10 @@ class Shift {
     
     const updateShift = () => {
      
-      
+      PAY_RATE = parseFloat(this.$shift_pay_rate.value)
 
-      this.shift_start = this.$shift_start_input .value.split(':');
-      this.shift_end   = this.$shift_end_input   .value.split(':');
+      this.shift_start = this.$shift_start_input .value.split(':')
+      this.shift_end   = this.$shift_end_input   .value.split(':')
       
       let start_time = parseFloat(this.shift_start[0]) + 
                           parseFloat(this.shift_start[1])/60.0
@@ -32,10 +32,10 @@ class Shift {
       
       
       if (end_time > start_time)
-        this.shift_hours = end_time - start_time  
+        this.shift_hours = (end_time - start_time)
       
       else if (start_time > end_time)
-        this.shift_hours = (end_time + 24) - start_time
+        this.shift_hours = ((end_time + 24) - start_time)
       
       else if (start_time == end_time)
         alert('Start time cannot equal end time')
@@ -54,33 +54,33 @@ class Shift {
     this.shift_start = '';
     this.shift_end   = '';
   }
-}
+}3
 
 
 class PayReportTable {    
   
   constructor(parent_workday) {
     
-    this.parent_workday       = parent_workday;
-    this.$regular_hours_cell  = $('#reg_hours')
-    this.$regular_pay_cell    = $('#reg_pay')
-    this.$overtime_hours_cell = $('#ot_hours')
-    this.$overtime_pay_cell   = $('#ot_pay')
-    this.$total_hours_cell    = $('#tot_hours')
-    this.$total_pay_cell      = $('#tot_pay')
-    this.$avg_payrate_cell    = $('#avg_payrate')
+    this.parent_workday       = parent_workday;    
+    this.$regular_hours_cell  = document.querySelector('#reg_hours')    
+    this.$regular_pay_cell    = document.querySelector('#reg_pay')    
+    this.$overtime_hours_cell = document.querySelector('#ot_hours')    
+    this.$overtime_pay_cell   = document.querySelector('#ot_pay')    
+    this.$total_hours_cell    = document.querySelector('#tot_hours')
+    this.$total_pay_cell      = document.querySelector('#tot_pay')    
+    this.$avg_payrate_cell    = document.querySelector('#avg_payrate')
   }
 
   updateTable() {
   
-    this.$regular_hours_cell  .text(    this.parent_workday.regular_hours  .toFixed(2))
-    this.$regular_pay_cell    .text(`$${this.parent_workday.regular_pay    .toFixed(2)}`)      
+    this.$regular_hours_cell  .textContent =     this.parent_workday.regular_hours  .toFixed(2)
+    this.$regular_pay_cell    .textContent = `$${this.parent_workday.regular_pay    .toFixed(2)}`
     
-    this.$overtime_hours_cell .text(    this.parent_workday.overtime_hours .toFixed(2))
-    this.$overtime_pay_cell   .text(`$${this.parent_workday.overtime_pay   .toFixed(2)}`)      
+    this.$overtime_hours_cell .textContent =     this.parent_workday.overtime_hours .toFixed(2)
+    this.$overtime_pay_cell   .textContent = `$${this.parent_workday.overtime_pay   .toFixed(2)}`
     
-    this.$total_hours_cell    .text(    this.parent_workday.total_hours    .toFixed(2))      
-    this.$total_pay_cell      .text(`$${this.parent_workday.total_pay      .toFixed(2)}`)
+    this.$total_hours_cell    .textContent =     this.parent_workday.total_hours    .toFixed(2)
+    this.$total_pay_cell      .textContent = `$${this.parent_workday.total_pay      .toFixed(2)}`
   }
 }
 
