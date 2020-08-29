4a3fd0f2fec2fea4ed7060d58bc3b84f0611b8bf
Adam Williams <adamthewilliams@gmail.com>
2020-08-29 06:01:41 -0500

    removed all jquery from index.js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,16 @@
 
 const OT_LIMIT_HOURS   = 5
 
-const poop = 'THIS IS A TEST'
 
 let   CURRENT_WORKDAY  = new Workday()
 let   MAIN_SHIFT       = new Shift(CURRENT_WORKDAY)
 
+  
+let register_link = document.createElement()
+register_link.attr('href') = './register.php'
+register_link.attr('id')   = 'register_link'
+register_link.textContent  = 'Register'
 
-$('document').ready(() => {
-
-
-  insert_register_link()
- 
-})
   
 
 
