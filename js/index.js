b94737cf531c938f44fae9d1be82a4672a786f53
Adam Williams <adamthewilliams@gmail.com>
2020-08-29 06:01:41 -0500

    removed all jquery from index.js

--- js/index.js
+++ js/index.js
@@ -6,13 +6,8 @@ let   CURRENT_WORKDAY  = new Workday()
 let   MAIN_SHIFT       = new Shift(CURRENT_WORKDAY)
 
   
-let register_link = document.createElement('a')
-register_link.href = './register.php'
-register_link.id   = 'register_link'
-
-$('#hourly_rate_input').focusout(() => {
-  CURRENT_WORKDAY.insertShiftAndCalculatePayBreakdown();  
-})
-
+let register_link = document.createElement()
+register_link.attr('href') = './register.php'
+register_link.attr('id')   = 'register_link'
 register_link.textContent  = 'Register'
 document.querySelector('footer').prepend(register_link)
