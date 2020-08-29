b927913681da54b4d452545e157d98e6c2877426
Adam Williams <adamthewilliams@gmail.com>
2020-08-29 06:13:07 -0500

    successfully removed all jquery

--- js/index.js
+++ js/index.js
@@ -6,10 +6,13 @@ let   CURRENT_WORKDAY  = new Workday()
 let   MAIN_SHIFT       = new Shift(CURRENT_WORKDAY)
 
   
-let register_link = document.createElement()
-register_link.attr('href') = './register.php'
-register_link.attr('id')   = 'register_link'
+let register_link = document.createElement('a')
+register_link.href = './register.php'
+register_link.id   = 'register_link'
+
+
 register_link.textContent  = 'Register'
+document.querySelector('footer').prepend(register_link)
 
   
 
